1f59eba21742979050d0c089041e342b397820a4
Jacques Beeets <jacques@hellofirstview.com>
2023-02-06 21:38:13 +0200

    dynamic list redering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,6 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 function Expenses(props) {
-  const [expenses, setExpensesState] = useState(props.expenses);
   const [filterYear, setFilterYear] = useState('2020');
 
   const updateFilterYear = (selectedYear) => {
@@ -14,30 +13,24 @@ function Expenses(props) {
 
   return (
     <Card className="expenses">
+
       <ExpenseFilter
         onFilterYearChange={updateFilterYear}
         filterByYear={filterYear}
       />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+
+      {/* Return Expense Items */}
+      {props.expenses.map((expense) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+       
     </Card>
   );
 }
