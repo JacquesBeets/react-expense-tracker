34a55ae75d0d2c00363ed9129f42ea974b4f189e
Jacques Beeets <jacques@hellofirstview.com>
2023-02-06 20:21:10 +0200

    prop functions

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -4,9 +4,17 @@ import "./NewExpense.css"
 
 const NewExpense = (props) => {
 
+  const saveExpenseDataHandler = (expenseData) => {
+    const data = {
+      ...expenseData,
+      id: Math.random().toString()
+    }
+    props.onAddExpense(data)
+  }
+
   return (
     <div className="new-expense">
-        <ExpenseForm />
+        <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
     </div>
   );
 }
